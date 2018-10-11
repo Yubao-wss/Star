299919a0e1dfbec3f58ab6c1f77fbbb23b323544
Yubao-wss <43661069+Yubao-wss@users.noreply.github.com>
2018-10-11 22:34:38 +0800

    打印乘法口诀

---
+++ 14.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	for (i = 1; i <= 9; i++)
+	{
+		{ 
+			for (j = 1; j <= i; j++)
+		printf("%d*%d=%2d  ", i, j, i*j);
+			printf("\n");
+		}
+	}
+	system("pause");
+	return 0;
+}
